8fe1061af1fb4e47528ff5139765219acbcfce6c
moonhyuk <godmoon00@naver.com>
2020-10-02 13:46:11 +0900

    #3 State

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const movies = [
-  {
-    id: 1,
-    name: "D-war",
-    img:
-      "https://upload.wikimedia.org/wikipedia/en/9/91/Dragon_Wars_poster.jpg",
-    rate: 0.1,
-  },
-  {
-    id: 2,
-    name: "Avengers",
-    img:
-      "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
-    rate: 4.0,
-  },
-  {
-    id: 3,
-    name: "The Dark Knight",
-    img:
-      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
-    rate: 4.5,
-  },
-];
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-function Movie({ name, img, rate }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <img src={img} alt={name}></img>
-      <h2>{rate}/5.0</h2>
-    </div>
-  );
-}
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 2000);
+  }
 
-Movie.propTypes = {
-  name: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
-  rate: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div>
-      {movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          name={movie.name}
-          img={movie.img}
-          rate={movie.rate}
-        />
-      ))}
-    </div>
-  );
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        <h1>{isLoading ? "Loading..." : "Done"}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
